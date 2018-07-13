d8bcb89467caf6c8e638b8e4fea3ff5087ca1424
Francesco De Lisi <francesco@intelligenttrading.org>
2018-07-13 18:16:44 +0700

    [FIX] Starter + ITF team was not giving access to the switch

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -11,10 +11,10 @@
         </el-row>
         <hr style="opacity:0.2;" />
       <div class="option-label">Indicators</div>
-      <el-row :gutter="24" :class="[{disabledIndicator: subscriptionPlan.plan != 'Starter' && !indicator.available},'indicator']" v-for="indicator in this.indicators" v-bind:key="indicator.name">
-            <el-col class="setting-label" :span="subscriptionPlan.plan != 'Starter' && !indicator.available ? 16 : 20">{{indicator.label}}</el-col>
-            <el-col :span="4"><el-button type="text" class="proTag" size=mini v-show="subscriptionPlan.plan != 'Starter' && !indicator.available" v-on:click="goToUpgrade">Upgrade</el-button></el-col>
-            <el-col :span="4"><el-switch :disabled="subscriptionPlan.plan != 'Starter' && !indicator.available" v-model="indicator.enabled" @change="save"></el-switch></el-col>
+      <el-row :gutter="24" :class="[{disabledIndicator: !subscriptionPlan.plan.includes('Starter') && !indicator.available},'indicator']" v-for="indicator in this.indicators" v-bind:key="indicator.name">
+            <el-col class="setting-label" :span="!subscriptionPlan.plan.includes('Starter') && !indicator.available ? 16 : 20">{{indicator.label}}</el-col>
+            <el-col :span="4"><el-button type="text" class="proTag" size=mini v-show="!subscriptionPlan.plan.includes('Starter') && !indicator.available" v-on:click="goToUpgrade">Upgrade</el-button></el-col>
+            <el-col :span="4"><el-switch :disabled="!subscriptionPlan.plan.includes('Starter') && !indicator.available" v-model="indicator.enabled" @change="save"></el-switch></el-col>
         </el-row>
     </el-main>
 </el-container>
